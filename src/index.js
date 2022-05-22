179ee8a2815c76d974e564de8ca150bbdb221e7e
salimov333 <salimov333@gmail.com>
2022-05-22 18:17:26 +0200

    basic web-app

---
+++ src/index.js
@@ -0,0 +1,65 @@
+//console.log("Test index.js");
+
+
+//Define the HTML Elements
+const city = document.getElementById("city");
+//console.log(city);
+const resultDiv = document.getElementById("result");
+//console.log(resultDiv);
+const btn = document.getElementById("btn");
+//console.log(btn);
+
+//Click enent on btn
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather();
+});
+
+//Onload method
+window.onload = () => {
+    //console.log("Page is loaded");
+    document.getElementById("city").focus();
+    city.setAttribute("placeholder", "");
+    city.placeholder = "For example: Berlin"
+};
+
+function getWeather() {
+    let cityName = city.value;
+    //console.log("cityName = ", cityName);
+    if (!cityName) {
+        //console.log("Enter a city name");
+        city.placeholder = "Enter s valid City name!"
+        return;
+    }
+    cityName = city.value;
+
+    //Weather API
+    const API_KEY = "5fe8d9f6fdc543a38b673009221905";
+    const baseURL = "http://api.weatherapi.com/v1";
+    const endPoint = `${baseURL}/current.json?key=${API_KEY}&q=${cityName}`;
+
+    axios
+        .get(endPoint)
+        .then((res) => {
+            const data = res.data;
+            console.log(data);
+            const img = document.getElementById("img");
+            const src = data.current.condition.icon;
+            img.setAttribute("src", src);
+            const name = data.location.name;
+            const country = data.location.country;
+            const temp_c = res.data.current.temp_c;
+            const condition = res.data.current.condition.text;
+            const wind_kph = res.data.current.wind_kph;
+            const lastUpdated = res.data.current.last_updated;
+            const hour = `${new Date(lastUpdated).getHours()}:${new Date(
+                lastUpdated
+            ).getMinutes()}`;
+            const resultText = `The weather today in ${name}/${country} at ${hour} is ${condition}. The temperature is ${temp_c}°C and the wind speed is ${wind_kph} km/h.`;
+            console.log(resultText);
+            resultDiv.innerHTML = resultText;
+            city.value = "";
+            city.focus();
+        })
+        .catch((err) => console.log(err));
+};
